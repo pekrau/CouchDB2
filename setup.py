db6ff59bd2f1193b2e405384e00ba846bec95907
Per Kraulis <per.kraulis@gmail.com>
2022-02-17 16:44:01 +0100

    black'ed. Minor change.

--- setup.py
+++ setup.py
@@ -3,34 +3,30 @@ from setuptools import setup
 import couchdb2
 
 
-setup(name="CouchDB2",
-      version=couchdb2.__version__,
-      description=couchdb2.__doc__[0],
-      long_description=open("README.md", "r").read(),
-      long_description_content_type="text/markdown",
-      url="https://github.com/pekrau/CouchDB2",
-      author="Per Kraulis",
-      author_email="per.kraulis@gmail.com",
-      license="MIT",
-      python_requires=">= 3.6",
-      py_modules=["couchdb2"],
-      install_requires=[
-          "requests>=2.26",
-          "tqdm>=4.62",
-      ],
-      entry_points={
-          "console_scripts": ["couchdb2=couchdb2:main"]
-      },
-      classifiers=[
-          "License :: OSI Approved :: MIT License",
-          "Intended Audience :: Developers",
-          "Natural Language :: English",
-          "Development Status :: 3 - Alpha",
-          "Programming Language :: Python :: 3 :: Only",
-          "Programming Language :: Python :: 3.6",
-          "Operating System :: OS Independent",
-          "Environment :: Console",
-          "Topic :: Database :: Front-Ends",
-          "Topic :: Software Development :: Libraries :: Python Modules"
-      ],
+setup(
+    name="CouchDB2",
+    version=couchdb2.__version__,
+    description=couchdb2.__doc__[0],
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pekrau/CouchDB2",
+    author="Per Kraulis",
+    author_email="per.kraulis@gmail.com",
+    license="MIT",
+    python_requires=">= 3.6",
+    py_modules=["couchdb2"],
+    install_requires=["requests>=2.26", "tqdm>=4.62"],
+    entry_points={"console_scripts": ["couchdb2=couchdb2:main"]},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Database :: Front-Ends",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
